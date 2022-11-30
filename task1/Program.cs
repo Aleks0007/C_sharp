643cd1407d9bed7a86401f614af8f691c93f2202
AlekseiShvaiko <aarkaklim@gmail.com>
2022-11-30 20:46:59 +0400

    Выполнена задача №2

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2)
+{
+    Console.Write("max = ");
+    Console.Write(number1);
+}
+if (number1 < number2)
+{
+    Console.Write("max = ");
+    Console.Write(number2);
+}
+if (number1 == number2)
+{
+    Console.Write("Оба числа равны, max = ");
+    Console.Write(number1);
+}
