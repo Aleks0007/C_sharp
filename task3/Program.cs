dee66470a466e031e26c4d5bcdaaa7c45024f669
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-01 14:55:48 +0400

    Выполнена задача №6

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+{
+Console.Write("Число является чётным");
+}
+else
+{
+Console.Write("Число является нечётным");
+}
