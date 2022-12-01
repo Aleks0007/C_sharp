02438012daf2d9380bde02fe1c5c98a8575df2c5
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-01 15:36:38 +0400

    Выполнена задача №8

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+while (i < number)
+{
+    Console.Write(i + 1);
+    Console.Write(" ");
+    i += 2;
+}
